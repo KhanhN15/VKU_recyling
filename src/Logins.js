5780495b6e71601ea57fc992c27b41977dc5888e
cuonglevan23 <103936303+cuonglevan23@users.noreply.github.com>
2022-09-24 00:08:12 +0700

    update new code

---
+++ src/Logins.js
@@ -0,0 +1,14 @@
+import Header from "./component/Header";
+
+
+const Logins = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+
+    </>
+  );
+};
+
+export default Logins;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,63 +1,65 @@
-import OutLineBg from "../OutLineBg";
+import Logins from "../Logins";
 
 const Login = () => {
     return (
         <>
-            <OutLineBg>
+            <Logins>
                 <div>
-                    <div className="container">
-                        <div className="forms-container">
-                            <div className="signin-signup">
-                                <form action="index.html" className="sign-in-form">
-                                    <h2 className="title">Đăng nhập</h2>
-                                    <div className="input-field">
-                                        <i className="fa-solid fa-user" />
-                                        <input type="text" placeholder="Tên đăng nhập" />
-                                    </div>
-                                    <div className="input-field">
-                                        <i className="fa-solid fa-lock" />
-                                        <input type="password" placeholder="Mật khẩu" />
-                                    </div>
-                                    <input type="submit" defaultValue="Đăng nhập" className="btn solid" href="doidiem.html" />
-                                    <p className="social-text">Hoặc đăng nhập bằng hình thức khác</p>
-                                    <div className="social-media">
-                                        <a href="#" className="social-icon"><i className="fa-brands fa-facebook" /></a>
-                                        <a href="#" className="social-icon"><i className="fa-brands fa-twitter" /></a>
-                                        <a href="#" className="social-icon"><i className="fa-brands fa-google" /></a>
-                                        <a href="#" className="social-icon"><i className="fa-brands fa-instagram-square" /></a>
-                                    </div>
-                                </form>
-                                <form action="#" className="sign-up-form">
-                                    <h2 className="title">Đăng ký</h2>
-                                    <div className="input-field">
-                                        <i className="fas fa-user" />
-                                        <input type="text" placeholder="Tên đăng nhập" />
-                                    </div>
-                                    <div className="input-field">
-                                        <i className="fas fa-envelope" />
-                                        <input type="email" placeholder="Email" />
-                                    </div>
-                                    <div className="input-field">
-                                        <i className="fas fa-lock" />
-                                        <input type="password" placeholder="Mật khẩu" />
-                                    </div>
-                                    <input type="submit" className="btn" defaultValue="Đăng ký" />
-                                    <p className="social-text">Hoặc đăng nhập bằng hình thức khác</p>
-                                    <div className="social-media">
-                                        <a href="#" className="social-icon">
-                                            <i className="fab fa-facebook-f" />
-                                        </a>
-                                        <a href="#" className="social-icon">
-                                            <i className="fab fa-twitter" />
-                                        </a>
-                                        <a href="#" className="social-icon">
-                                            <i className="fab fa-google" />
-                                        </a>
-                                        <a href="#" className="social-icon">
-                                            <i className="fab fa-linkedin-in" />
-                                        </a>
-                                    </div>
-                                </form>
+                    <div className="box-content">
+                        <div className="container">
+                            <div className="forms-container">
+                                <div className="signin-signup">
+                                    <form action="index.html" className="sign-in-form">
+                                        <h2 className="title">Đăng nhập</h2>
+                                        <div className="input-field">
+                                            <i className="fa-solid fa-user" />
+                                            <input type="text" placeholder="Tên đăng nhập" />
+                                        </div>
+                                        <div className="input-field">
+                                            <i className="fa-solid fa-lock" />
+                                            <input type="password" placeholder="Mật khẩu" />
+                                        </div>
+                                        <input type="submit" defaultValue="Đăng nhập" className="btn solid" href="doidiem.html" />
+                                        {/* <p class="social-text">Hoặc đăng nhập bằng hình thức khác</p>
+                  <div class="social-media">
+                      <a href="#" class="social-icon"><i class="fa-brands fa-facebook"></i></a>
+                      <a href="#" class="social-icon"><i class="fa-brands fa-twitter"></i></a>
+                      <a href="#" class="social-icon"><i class="fa-brands fa-google"></i></a>
+                      <a href="#" class="social-icon"><i class="fa-brands fa-instagram-square"></i></a>
+                  </div> */}
+                                    </form>
+                                    <form action="#" className="sign-up-form">
+                                        <h2 className="title">Đăng ký</h2>
+                                        <div className="input-field">
+                                            <i className="fas fa-user" />
+                                            <input type="text" placeholder="Tên đăng nhập" />
+                                        </div>
+                                        <div className="input-field">
+                                            <i className="fas fa-envelope" />
+                                            <input type="email" placeholder="Email" />
+                                        </div>
+                                        <div className="input-field">
+                                            <i className="fas fa-lock" />
+                                            <input type="password" placeholder="Mật khẩu" />
+                                        </div>
+                                        <input type="submit" className="btn" defaultValue="Đăng ký" />
+                                        {/* <p class="social-text">Hoặc đăng nhập bằng hình thức khác</p>
+                  <div class="social-media">
+                      <a href="#" class="social-icon">
+                          <i class="fab fa-facebook-f"></i>
+                      </a>
+                      <a href="#" class="social-icon">
+                          <i class="fab fa-twitter"></i>
+                      </a>
+                      <a href="#" class="social-icon">
+                          <i class="fab fa-google"></i>
+                      </a>
+                      <a href="#" class="social-icon">
+                          <i class="fab fa-linkedin-in"></i>
+                      </a>
+                  </div> */}
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -76,7 +78,9 @@ const Login = () => {
                     </div>
                 </div>
 
-            </OutLineBg>
+
+
+            </Logins>
         </>
     );
 }
